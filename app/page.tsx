969625997f08c34d2d5666a4cd8b977e7295e266
Maxime Moreillon <m.moreillon@gmail.com>
2024-12-22 16:02:47 +0900

    nested states

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,7 @@ import Trousers from "./garments/trousers"
 import { svgHeight, svgWidth } from "./params"
 
 export default function Home() {
-  const [trousers, setTrousers] = useState(true)
-  const [trousersColor, setTrousersColor] = useState("#c00000")
+  const [trousers, setTrousers] = useState({ show: true, color: "#c00000" })
   const [dressShirt, setDressShirt] = useState(true)
   const [blazer, setBlazer] = useState(true)
 
@@ -21,17 +20,23 @@ export default function Home() {
           height={svgHeight}
           className="border-red-600 border-2"
         >
-          {trousers && <Trousers style={{ fill: trousersColor }} />}
+          {trousers.show && <Trousers style={{ fill: trousers.color }} />}
           {dressShirt && <DressShirt style={{ fill: "white" }} />}
           {blazer && <Blazer style={{ fill: "navy" }} />}
         </svg>
         <div className="flex flex-col gap-2 items-start p-2">
           <div>
-            <button onClick={() => setTrousers(!trousers)}>Trousers</button>
+            <button
+              onClick={() => setTrousers({ ...trousers, show: !trousers.show })}
+            >
+              Trousers
+            </button>
             <input
               type="color"
-              value={trousersColor}
-              onChange={(e) => setTrousersColor(e.target.value)}
+              value={trousers.color}
+              onChange={(e) =>
+                setTrousers({ ...trousers, color: e.target.value })
+              }
             />
           </div>
           <button onClick={() => setBlazer(!blazer)}>Blazer</button>
